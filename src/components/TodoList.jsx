666f82c41b9c732ff3c99613faa7524a866f4b1e
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-07-24 12:43:06 +0530

    update ui for todo list view

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,16 @@
 import { FiEdit2, FiTrash2 } from "react-icons/fi";
 import "./TodoList.css";
 
+const lightColors = [
+  "#eff6ff",
+  "#ecfdf5",
+  "#fffbeb",
+  "#fdf2f8",
+  "#f5f3ff",
+  "#f0fdfa",
+  "#fff7ed",
+];
+
 export default function TodoList({
   todos,
   deleteTodo,
@@ -11,15 +21,17 @@ export default function TodoList({
   if (isLoading) {
     return (
       <div className="list-container">
-        {[...Array(4)].map((_, index) => (
+        {[...Array(6)].map((_, index) => (
           <div className="list-item skeleton-item" key={index}>
-            <div className="left-side">
-              <div className="skeleton skeleton-checkbox"></div>
+            <div className="card-top">
               <div className="skeleton skeleton-text"></div>
             </div>
-            <div className="right-side">
-              <div className="skeleton skeleton-btn"></div>
-              <div className="skeleton skeleton-btn"></div>
+            <div className="card-bottom">
+              <div className="skeleton skeleton-tag"></div>
+              <div className="card-actions">
+                <div className="skeleton skeleton-btn"></div>
+                <div className="skeleton skeleton-btn"></div>
+              </div>
             </div>
           </div>
         ))}
@@ -31,36 +43,50 @@ export default function TodoList({
     <div className="list-container">
       {todos.length > 0 ? (
         todos.map((todo) => {
+          const bgColor = lightColors[todo.id % lightColors.length];
+
           return (
-            <div className="list-item" key={todo.id}>
-              <div className="left-side">
+            <div
+              className="list-item"
+              key={todo.id}
+              style={{ backgroundColor: bgColor }}
+            >
+              <div className="card-top">
+                <p className={todo.completed ? "completed" : ""}>
+                  {todo.todoName}
+                </p>
                 <input
                   type="checkbox"
                   className="todo-checkbox"
                   checked={todo.completed}
                   onChange={() => handleToggleTodo(todo.id)}
                 />
-                <p className={todo.completed ? "completed" : ""}>
-                  {todo.todoName}
-                </p>
               </div>
 
-              <div className="right-side">
-                <button
-                  className="icon-btn edit-btn"
-                  onClick={() => editTodo(todo.id)}
-                  disabled={todo.completed}
-                  data-tooltip="Edit"
+              <div className="card-bottom">
+                <span
+                  className={`todo-status ${todo.completed ? "is-completed" : "is-pending"}`}
                 >
-                  <FiEdit2 />
-                </button>
-                <button
-                  className="icon-btn delete-btn"
-                  onClick={() => deleteTodo(todo.id)}
-                  data-tooltip="Delete"
-                >
-                  <FiTrash2 />
-                </button>
+                  {todo.completed ? "Completed" : "Pending"}
+                </span>
+
+                <div className="card-actions">
+                  <button
+                    className="icon-btn edit-btn"
+                    onClick={() => editTodo(todo.id)}
+                    disabled={todo.completed}
+                    data-tooltip="Edit"
+                  >
+                    <FiEdit2 />
+                  </button>
+                  <button
+                    className="icon-btn delete-btn"
+                    onClick={() => deleteTodo(todo.id)}
+                    data-tooltip="Delete"
+                  >
+                    <FiTrash2 />
+                  </button>
+                </div>
               </div>
             </div>
           );
